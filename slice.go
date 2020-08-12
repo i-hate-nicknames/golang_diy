f47b7148ce2068adabed42f322c666264b33e2a5
nvm <nevermn@protonmail.com>
2020-08-12 11:13:16 +0300

    Add slice interface

---
+++ slice.go
@@ -0,0 +1,11 @@
+package main
+
+type IntSlice interface {
+	Get(index int) (int, error)
+	Set(index, val int) error
+	Slice(start, end int) error
+	Len() int
+	Cap() int
+	Append(other IntSlice) IntSlice
+	Copy() IntSlice
+}
